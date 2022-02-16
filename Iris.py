07888285c418fc7459ce65f5402b9b246c03c49f
Jeet <98499268+JeetRH@users.noreply.github.com>
2022-02-16 10:58:44 -0500

    Update Iris.py

--- Iris.py
+++ Iris.py
@@ -4,8 +4,7 @@ import pickle
 class Iris:
   def __init__(self):
     self.model_name = "Iris"
-    model = pickle.load(open("finalized_model.sav", 'rb'))
-    pred = model.predict(df)
+    self.model = pickle.load(open("finalized_model.sav", 'rb'))
     
   def predict(self, X, features_names):
     #logging.info(f"Got request {X} with features {features_names}")        
@@ -13,5 +12,6 @@ class Iris:
     # load the model from disk
     #loaded_model = pickle.load(open(filename, 'rb'))
     #result = loaded_model.score(X_test, Y_test)
+    pred = self.model.predict(df)
     #print(result)
     return pred
