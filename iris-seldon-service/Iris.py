8bb84290838decb91bb86d09277bcb02ca715f5f
Trevor Royer <troyer@redhat.com>
2022-02-17 08:56:46 -0600

    cleanup comments

--- iris-seldon-service/Iris.py
+++ iris-seldon-service/Iris.py
@@ -5,14 +5,12 @@ import pickle
 class Iris:
     def __init__(self):
         self.model_name = "Iris"
+
+        # load the model from disk
         self.model = pickle.load(open("finalized_model.sav", "rb"))
 
     def predict(self, X, features_names):
-        # logging.info(f"Got request {X} with features {features_names}")
         df = pd.DataFrame(data=X, columns=features_names)
-        # load the model from disk
-        # loaded_model = pickle.load(open(filename, 'rb'))
-        # result = loaded_model.score(X_test, Y_test)
+        
         pred = self.model.predict(df)
-        # print(result)
         return pred
