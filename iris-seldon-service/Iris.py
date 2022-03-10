d0f292bc2ff044ccc788e8a478f4972e2884e3ce
Trevor Royer <troyer@redhat.com>
2022-03-10 09:45:23 -0600

    add dummy metrics

--- iris-seldon-service/Iris.py
+++ iris-seldon-service/Iris.py
@@ -14,3 +14,10 @@ class Iris:
         
         pred = self.model.predict(df)
         return pred
+
+    def metrics(self):
+        return [
+            {"type": "COUNTER", "key": "mycounter", "value": 1}, # a counter which will increase by the given value
+            {"type": "GAUGE", "key": "mygauge", "value": 100},   # a gauge which will be set to given value
+            {"type": "TIMER", "key": "mytimer", "value": 20.2},  # a timer which will add sum and count metrics - assumed millisecs
+        ]
